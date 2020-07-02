2bcf48cf055e7e4c955a3f8959d9ada664f59d8c
Singgih <51188767+afdolsing@users.noreply.github.com>
2020-07-02 11:50:27 +0700

    database

---
+++ db_todo.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 02, 2020 at 06:50 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_todo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_todo`
+--
+
+CREATE TABLE `tbl_todo` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_todo`
+--
+
+INSERT INTO `tbl_todo` (`id`, `name`, `date`) VALUES
+(1, 'Learn Web Development', 'Thursday, 02 July 2020'),
+(2, 'Learn PHP OOP', 'Thursday, 02 July 2020'),
+(4, 'Learn jQuery', 'Thursday, 02 July 2020');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_todo`
+--
+ALTER TABLE `tbl_todo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_todo`
+--
+ALTER TABLE `tbl_todo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
